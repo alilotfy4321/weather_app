73e26cbc11b9e0fa0be32748fda011b30db690fb
alilotfy4321 <alilotfy4321@gmail.com>
2024-12-05 15:06:07 +0200

    change dark mode color

--- lib/core/styles/themes/dark_theme.dart/app_dark_colors.dart
+++ lib/core/styles/themes/dark_theme.dart/app_dark_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppDarkColors {
   //-----------------appBar-----
-  static const appBarColor = Color.fromARGB(255, 146, 138, 138);
+  static const appBarColor = Color.fromARGB(255, 71, 66, 66);
   static const appBartitleTextColor = Colors.white;
   static const appBarIconColor = Colors.white;
 }
